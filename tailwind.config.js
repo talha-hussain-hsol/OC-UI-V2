1082dd2841a75db90440a7a30628975a7675a606
hamza-atif-hsol <hamza@heuristicsol.com>
2024-09-10 11:43:44 +0500

    apply styling for dark theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,8 @@ module.exports = {
           "linear-gradient(to right, #243279 0%, #0091BB 100%)",
         "color-cardheader-stepper-SC":
           "linear-gradient(to bottom, #24357C 0%, #0B76A8 100%)", // Card header in stepper ex: Face Snap
+          "gradient-stepper-card-Ascent":
+          "linear-gradient(to bottom, #0d345d 0%, #151c39 100%)",
       },
       backgroundColor: {
         "color-SC": "#edf2f4",
@@ -56,6 +58,8 @@ module.exports = {
         "color-barchart-SC": "#0D7EF9",
         "color-barchart1-SC": "#68D930",
         "color-stepper-proceedbutton-SC": "#F0082E",
+        "color-stepper-proceedbutton-Ascent": "rgb(230, 55, 87)",
+
         "color-stepstatus-SC": "#24357C", // For Summary Page -> Status
         "sidebar-color-SC": "#FFFFFF",
         "color-stepper-card-SC": "#23367C",
