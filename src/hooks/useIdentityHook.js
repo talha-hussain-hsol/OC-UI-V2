371d42d1f8abd1f1605df581e80e502b2f09edd7
Arooba Masood <arooba-masood-hsol.com>
2024-09-25 17:35:05 +0500

    changes in useidentityhook

--- src/hooks/useIdentityHook.js
+++ src/hooks/useIdentityHook.js
@@ -1,75 +1,29 @@
-// import { useCallback, useEffect, useState } from "react";
-// import { getIdentityList } from "../api/userApi";
-// import { toast } from "react-toastify";
-// import axios from "axios";
-// import { setLocalStorage } from "../utils/cookies";
-// import { useNavigate } from "react-router-dom";
-// import useEntityStore from "../store/useEntityStore";
-
-// const portals = [
-//   { label: "Customer Portal", type: "customer" },
-//   { label: "Compliance Portal", type: "compliance" },
-//   { label: "Manager Portal", type: "management" },
-// ];
-
-// const useIdentityHook = () => {
-//   const [entites, setEntites] = useState([]);
-//   const [activePortal, setActivePortal] = useState(portals[0].type);
-//   const [isLoader, setIsLoader] = useState(false);
-//   const cancelTokenSource = axios.CancelToken.source();
-
-  
-//   const handleGetIdentityList = useCallback(async () => {
-//     console.log(`checking`);
-//     setIsLoader(true);
-
-//     const response = await getIdentityList(cancelTokenSource.token);
-//     console.log("object 1", response);
-//     if (response == true) {
-//       setIsLoader(false);
-//       setProfileListData(response?.data);
-//     } else {
-//       setIsLoader(false);
-//     }
-//   }, []);
-
-//   useEffect(() => {
-//     handleGetIdentityList();
-//   }, [handleGetIdentityList]);
-
-//   const handleActivePortal = useCallback((portalType) => {
-//     setActivePortal(portalType);
-//   }, []);
-
-  
-
-//   return {
-//     entites,
-//     activePortal,
-//     portals,
-//     handleActivePortal,
-//     isLoader
-//   };
-// };
-
-// export default useIdentityHook;
-
-
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
 import { getIdentityList } from "../api/userApi";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { setLocalStorage } from "../utils/cookies";
+import { useNavigate } from "react-router-dom";
+import useEntityStore from "../store/useEntityStore";
 
-const useIdentities = () => {
-  const [profileListData, setProfileListData] = useState([]);
+const portals = [
+  { label: "Customer Portal", type: "customer" },
+  { label: "Compliance Portal", type: "compliance" },
+  { label: "Manager Portal", type: "management" },
+];
+
+const useIdentityHook = () => {
+  const [entites, setEntites] = useState([]);
+  const [activePortal, setActivePortal] = useState(portals[0].type);
   const [isLoader, setIsLoader] = useState(false);
-  const [status, setStatus] = useState([]);
   const cancelTokenSource = axios.CancelToken.source();
 
-  
-  
   const handleGetIdentityList = useCallback(async () => {
+    console.log(`checking`);
     setIsLoader(true);
+
     const response = await getIdentityList(cancelTokenSource.token);
+    console.log("object 1", response);
     if (response == true) {
       setIsLoader(false);
       setProfileListData(response?.data);
@@ -82,17 +36,17 @@ const useIdentities = () => {
     handleGetIdentityList();
   }, [handleGetIdentityList]);
 
-  
   const handleActivePortal = useCallback((portalType) => {
     setActivePortal(portalType);
   }, []);
 
   return {
-    profileListData,
-    isLoader,
-    status,
-    handleToggleStatus,
+    entites,
+    activePortal,
+    portals,
+    handleActivePortal,
+    isLoader
   };
 };
 
-export default useIdentities;
+export default useIdentityHook;
