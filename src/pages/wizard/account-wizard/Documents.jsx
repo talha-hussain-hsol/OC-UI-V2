e63d9d74b28960933cce8971c93eed4257d6e95c
Hussain Syed <hussain.iqrar@heuristicsol.com>
2024-09-27 16:39:52 +0500

    Refference Docs Adjusted

--- src/pages/wizard/account-wizard/Documents.jsx
+++ src/pages/wizard/account-wizard/Documents.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import Button from "../../../components/ui/button/Button";
 import Modal from "../../../components/modal/Modal";
 import { useTheme } from "../../../contexts/themeContext";
