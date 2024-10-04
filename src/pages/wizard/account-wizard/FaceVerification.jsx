d770044d69ac4e20c3c3eeb05abb1ce44fddd20b
hamza-atif-hsol <hamza@heuristicsol.com>
2024-10-04 15:02:22 +0500

    Face and vcip get apis done

--- src/pages/wizard/account-wizard/FaceVerification.jsx
+++ src/pages/wizard/account-wizard/FaceVerification.jsx
@@ -630,8 +630,6 @@
 
 // export default FaceVerification;
 
-
-
 // // {props?.dataOfAccountSetup?.fund_data?.fund_setting?.account
 // //   ?.applicant?.identity?.indivisual?.provider?.verify?.face
 // //   ?.isBlured && (
@@ -640,8 +638,6 @@
 // //     Blurred.
 // //   </h5>
 // // )}
-
-
 
 import React, { useState, useEffect } from "react";
 import Button from "../../../components/ui/button/Button";
@@ -653,7 +649,6 @@ import {
 import axios from "axios";
 import Loader from "../../../components/ui/loader";
 import faceimg from "../../../assets/face.png";
-
 
 function FaceVerification(props) {
   const { theme } = useTheme();
@@ -672,21 +667,22 @@ function FaceVerification(props) {
         cancelTokenSource.token
       );
       setIsLoader(false);
-      
+
       if (response.success === true) {
-        const accountDetails = response?.data?.account_detail?.attach_identities?.[0]?.meta?.identities?.[
-          "53c8a88a-cc42-4da4-958b-71e79df5ad5f"
-        ]?.faceVerification;
-  
+        const accountDetails =
+          response?.data?.account_detail?.attach_identities?.[0]?.meta
+            ?.identities?.["53c8a88a-cc42-4da4-958b-71e79df5ad5f"]
+            ?.faceVerification;
+
         setApiResponse(accountDetails);
-        console.log("setApiResponse1",accountDetails)
-        console.log("setApiResponsess",apiResponse)
+        console.log("setApiResponse1", accountDetails);
+        console.log("setApiResponsess", apiResponse);
       }
     } catch (error) {
       if (axios.isCancel(error)) {
-        console.log('Request canceled:', error.message);
+        console.log("Request canceled:", error.message);
       } else {
-        console.error('Error fetching account details:', error);
+        console.error("Error fetching account details:", error);
       }
       setIsLoader(false);
     }
@@ -699,14 +695,14 @@ function FaceVerification(props) {
       };
       const response = await getDataSignedUrl(
         dataToSend,
-        cancelTokenSource.token,
+        cancelTokenSource.token
       );
       setIsLoader(false);
       if (response.success === true) {
-        console.log(response?.data, 'response?.data');
+        console.log(response?.data, "response?.data");
         setFace(faceimg);
       } else {
-        console.log(response, 'error');
+        console.log(response, "error");
       }
     }
     if (apiResponse?.images?.identification) {
@@ -716,124 +712,130 @@ function FaceVerification(props) {
       };
       const response = await getDataSignedUrl(
         dataToSend,
-        cancelTokenSource.token,
+        cancelTokenSource.token
       );
       setIsLoader(false);
       if (response.success === true) {
-        console.log(response?.data, 'response?.data');
+        console.log(response?.data, "response?.data");
         setIdentification(faceimg);
       } else {
-        console.log(response, 'error');
+        console.log(response, "error");
       }
     }
   };
-  useEffect(()=>{
+  useEffect(() => {
     handleGetAccountDetail();
-  },[])
+  }, []);
 
   useEffect(() => {
-  renderImages()
-   
+    renderImages();
   }, [apiResponse]);
 
   return (
     <>
-    {isLoader ? (
-          
-          <Loader/>
-          
-          ) : (
-            <>
-      <div className={`mt-2 border-b-4 border-color-${theme} px-2`}>
-      <h1 className="text-xl lg:text-2xl mb-2">Liveliness Test</h1>
-      <p className="text-sm lg:text-md mb-6 mt-4 uppercase">
-        To meet the regulatory KYC and AML/CFT requirements, this step helps
-        us to ensure that you are who you say you are.
-      </p>
-    </div>
-    <div className="flex flex-col lg:flex-row lg:space-x-8 mt-8 lg:mt-[74px] justify-center px-4">
-      {face || identification ? (
-        <>
-          {face ===null ? (
-            <div
-              className={`bg-color-stepper-card-${theme} p-6 rounded-lg w-full lg:w-[40%] shadow-[5px_5px_15px_5px_rgba(0,0,0,0.3)] mb-8 lg:mb-0`}
-            >
-              <h2
-                className={`bg-color-stepper-card-${theme} text-lg p-4 rounded-md mb-4 text-center shadow-[5px_5px_15px_5px_rgba(0,0,0,0.3)] uppercase`}
-              >
-                Face Verification
-              </h2>
-              <img
-                src={face}
-                alt=""
-                className="w-full h-auto rounded-lg"
-              />
-              
-            </div>
-          ):(
-            <>
-            <div
-              className={`bg-color-stepper-card-${theme} p-6 rounded-lg w-full lg:w-[40%] shadow-[5px_5px_15px_5px_rgba(0,0,0,0.3)] mb-8 lg:mb-0`}
-            >
-              <h2
-                className={`bg-color-stepper-card-${theme} text-lg p-4 rounded-md mb-4 text-center shadow-[5px_5px_15px_5px_rgba(0,0,0,0.3)] uppercase`}
-              >
-                Face Verification
-              </h2>
-              <div className="flex flex-col items-center mb-4 mt-36">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-24 w-24 text-sky-500"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M12 2a6 6 0 00-6 6v1H4a2 2 0 00-2 2v6a2 2 0 002 2h16a2 2 0 002-2v-6a2 2 0 00-2-2h-2V8a6 6 0 00-6-6zm-1 11a1 1 0 112 0v2a1 1 0 11-2 0v-2zm1-5a3 3 0 00-2.83 2H10a1 1 0 000 2h4a1 1 0 000-2h-.17A3 3 0 0012 8z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-                <p className="text-center text-sm lg:text-lg mt-10 uppercase">
-             Position your face within the designated area.
-           </p>
-              <Button
-                text="Proceed"
-                className={`bg-color-stepper-proceedbutton-${theme} hover:bg-red-600 text-white py-4 lg:py-[20px] px-4 rounded-lg mx-auto block mt-10`}
-              />
-              </div>
-              </div>
-            </>
-          )}
-          {identification && (
-            <div
-              className={`bg-color-stepper-card-${theme} p-6 rounded-lg w-full lg:w-[40%] shadow-[5px_5px_15px_5px_rgba(0,0,0,0.3)] mb-8 lg:mb-0`}
-            >
-              <h2
-                className={`bg-color-stepper-card-${theme} text-lg p-4 rounded-md mb-4 text-center shadow-[5px_5px_15px_5px_rgba(0,0,0,0.3)] uppercase`}
-              >
-                ID Card
-              </h2>
-              <img
-                src={identification}
-                alt=""
-                className="w-full h-auto rounded-lg"
-              />
-             
-            </div>
-          )}
-        </>
+      {isLoader ? (
+        <Loader />
       ) : (
-        <p>No images available.</p>
+        <>
+          <div className={`mt-2 border-b-4 border-color-${theme} px-2`}>
+            <h1 className="text-xl lg:text-2xl mb-2">Liveliness Test</h1>
+            <p className="text-sm lg:text-md mb-6 mt-4 uppercase">
+              To meet the regulatory KYC and AML/CFT requirements, this step
+              helps us to ensure that you are who you say you are.
+            </p>
+          </div>
+          <div className="flex flex-col lg:flex-row lg:space-x-8 mt-8 lg:mt-[74px] justify-center px-4">
+            {face || identification ? (
+              <>
+                <div
+                  className={`bg-color-stepper-card-${theme} p-6 rounded-lg w-full lg:w-[40%] shadow-[5px_5px_15px_5px_rgba(0,0,0,0.3)] mb-8 lg:mb-0`}
+                >
+                  <h2
+                    className={`bg-color-stepper-card-${theme} text-lg p-4 rounded-md mb-4 text-center shadow-[5px_5px_15px_5px_rgba(0,0,0,0.3)] uppercase`}
+                  >
+                    Face Verification
+                  </h2>
+                  {face !== null ? (
+                    <img
+                      src={face}
+                      alt=""
+                      className="w-full h-auto rounded-lg"
+                    />
+                  ) : (
+                    <>
+                      <div className="flex flex-col items-center mb-4 mt-20">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-24 w-24 text-sky-500"
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M12 2a6 6 0 00-6 6v1H4a2 2 0 00-2 2v6a2 2 0 002 2h16a2 2 0 002-2v-6a2 2 0 00-2-2h-2V8a6 6 0 00-6-6zm-1 11a1 1 0 112 0v2a1 1 0 11-2 0v-2zm1-5a3 3 0 00-2.83 2H10a1 1 0 000 2h4a1 1 0 000-2h-.17A3 3 0 0012 8z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                        <p className="text-center text-sm lg:text-lg mt-10 uppercase">
+                          Position your face within the designated area.
+                        </p>
+                        <Button
+                          text="Proceed"
+                          className={`bg-color-stepper-proceedbutton-${theme} hover:bg-red-600 text-white py-4 lg:py-[20px] px-4 rounded-lg mx-auto block mt-10`}
+                        />
+                      </div>
+                    </>
+                  )}
+                </div>
+                <div
+                  className={`bg-color-stepper-card-${theme} p-6 rounded-lg w-full lg:w-[40%] shadow-[5px_5px_15px_5px_rgba(0,0,0,0.3)] mb-8 lg:mb-0`}
+                >
+                  <h2
+                    className={`bg-color-stepper-card-${theme} text-lg p-4 rounded-md mb-4 text-center shadow-[5px_5px_15px_5px_rgba(0,0,0,0.3)] uppercase`}
+                  >
+                    National ID / Passport with Photo
+                  </h2>
+                  {identification !== null ? (
+                    <img
+                      src={identification}
+                      alt=""
+                      className="w-full h-auto rounded-lg"
+                    />
+                  ) : (
+                    <>
+                      <div className="flex flex-col items-center mb-4 mt-20">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-24 w-24 text-sky-500"
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M12 2a6 6 0 00-6 6v1H4a2 2 0 00-2 2v6a2 2 0 002 2h16a2 2 0 002-2v-6a2 2 0 00-2-2h-2V8a6 6 0 00-6-6zm-1 11a1 1 0 112 0v2a1 1 0 11-2 0v-2zm1-5a3 3 0 00-2.83 2H10a1 1 0 000 2h4a1 1 0 000-2h-.17A3 3 0 0012 8z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                        <p className="text-center text-sm lg:text-lg mt-10 uppercase">
+                          Position your face within the designated area.
+                        </p>
+                        <Button
+                          text="Proceed"
+                          className={`bg-color-stepper-proceedbutton-${theme} hover:bg-red-600 text-white py-4 lg:py-[20px] px-4 rounded-lg mx-auto block mt-10`}
+                        />
+                      </div>
+                    </>
+                  )}
+                </div>
+              </>
+            ) : (
+              <p>No images available.</p>
+            )}
+          </div>
+        </>
       )}
-    </div>
-    </>
-    )}
     </>
   );
 }
 
 export default FaceVerification;
-
-
-
-
